b5c03b627e1706af6a3a1a5fc9a2342dbad623ed
Carmen Ho <carmen.ho@cra-arc.gc.ca>
2018-08-31 18:20:50 +0000

    Add canada.ca template and carousel component

--- baseapp-frontend/src/app/app.module.ts
+++ baseapp-frontend/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {DemoService} from './demo.service'
 
-import { AppComponent }  from './app.component';
+import { AppComponent }  from './app.component';]
+import { CarouselComponent } from './carousel/carousel.component';]
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpClientModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, CarouselComponent],
     providers: [DemoService],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     bootstrap: [AppComponent]
