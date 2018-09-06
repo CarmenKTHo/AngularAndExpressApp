cc71a888fcd53e07dd89a08a0cff877e062d16fe
Carmen Ho <carmen.ho@cra-arc.gc.ca>
2018-09-06 12:50:20 +0000

    Add header and footer components

--- baseapp-frontend/src/app/app.module.ts
+++ baseapp-frontend/src/app/app.module.ts
@@ -7,10 +7,16 @@ import {MoreservicesandinfoService} from './moreservicesandinfo.service';
 import { AppComponent }  from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { MoreservicesandinfoComponent } from './moreservicesandinfo/moreservicesandinfo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpClientModule],
-    declarations: [AppComponent, CarouselComponent, MoreservicesandinfoComponent],
+    declarations: [AppComponent,
+      CarouselComponent,
+      MoreservicesandinfoComponent, 
+      HeaderComponent,
+      FooterComponent],
     providers: [MoreservicesandinfoService],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     bootstrap: [AppComponent]
